c27a3cd207241fede8a934e14870ec1c42312429
stressGC <g.cosson@live.fr>
2019-05-07 04:01:21 -0400

    comment & cleaning

--- src/main/fights/Generator.scala
+++ src/main/fights/Generator.scala
@@ -1,47 +1,61 @@
+/**
+  * @author Georges Cosson
+  */
 package fights
 
 import org.apache.spark.SparkContext
 import org.apache.spark.graphx.{Edge, Graph}
-
 import scala.collection.mutable.ArrayBuffer
 
+/**
+  * utility class used to generate various objects
+  */
 class Generator {
+  // some helpers
   val fightGraph = new GraphHelper()
   val mathHelper = new MathHelper()
 
+  /**
+    * returns a graph representing the fight #1
+    * @param sc SparkContext
+    * @return
+    */
   def generateFightOne(sc: SparkContext): Graph[Entity, Int] = {
 
-    //Definition des sommets
+    // lets get our vertices
     val vertices = this.getFightOneEntities()
+
+    // convert them as tuples
     val verticesAsTuples = fightGraph.getVerticesAsTuple(vertices)
 
-    //verticesAsTuples.foreach(x => println("\nID = " + x._1 + ", " + x._2.toString))
 
-    //Definition des arretes
+    // lets get our edges
     val edges = fightGraph.generateEdges(verticesAsTuples)
-    //println("edges :")
-    //edges.foreach(x => println(x.toString))
 
+    // finally return the Graph made from our vertices and edges
     return Graph(sc.makeRDD(verticesAsTuples), sc.makeRDD(edges))
 
   }
 
+  /**
+    * generates all the entities of the first fight
+    * @return ArrayBuffer of Entities
+    */
   def getFightOneEntities() : ArrayBuffer[Entity] = {
     /*
-      1 Solar
+      1 Solar : https://pathfinderwiki.com/wiki/Solar
       vs
       9 Worgs Rider
       4 Barbares Orcs
       1 Warlord
-
-       name healPoint healPointMax armor weaponArray x y peed regeneration
      */
 
+    // will contain our entities
     var entities = new ArrayBuffer[Entity]()
 
     /* 1x Solar */
-    val greatSword = new Spell(name = "Great Sword", range = 30, min = 21, max = 24)
-    val solar = new Entity(name = "Solar", health = 1364, armor = 44, regen = 15, speed = 50, spell = greatSword, coordX = mathHelper.getRandom(0, 500), coordY = mathHelper.getRandom(0, 500))
+    val lumenSword = new Spell(name = "Lumen Sword", range = 30, min = 21, max = 24)
+    val solar = new Entity(name = "Solar", health = 364, armor = 44, regen = 15, speed = 50, spell = lumenSword, coordX = mathHelper.getRandom(0, 500), coordY = mathHelper.getRandom(0, 500))
     entities += solar
 
     /* 9x Worgs Rider */
@@ -51,15 +65,17 @@ class Generator {
     })
 
     /* 4x Barbares Orcs */
-    val greatAxe = new Spell(name = "Great Axe", range = 10 , min = 11, max = 23)
-    (1 to 9) foreach (x => {
-      entities += new Entity(name = "Barbares Orcs #" + x, health = 42, armor = 15, regen = 0, speed = 30, spell = greatAxe, coordX = mathHelper.getRandom(0, 500), coordY = mathHelper.getRandom(0, 500))
+    val bigAxe = new Spell(name = "Big Axe", range = 10 , min = 11, max = 23)
+    (1 to 4) foreach (x => {
+      entities += new Entity(name = "Barbares Orcs #" + x, health = 42, armor = 15, regen = 0, speed = 30, spell = bigAxe, coordX = mathHelper.getRandom(0, 500), coordY = mathHelper.getRandom(0, 500))
     })
 
-    /* Warlord */
-/*
+    /* 1x Warlord */
+    val viciousFail = new Spell(name = "Viscious Fail", range = 10, min = 9, max = 18)
+    val warlord = new Entity(name = "Warlord", health = 141, armor = 27, regen = 0, speed = 30, spell = viciousFail, coordX = mathHelper.getRandom(0, 500), coordY = mathHelper.getRandom(0, 500))
+    entities += warlord
 
-*/
+    // return all those entities !
     return entities
   }
 }
